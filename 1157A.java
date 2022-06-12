f0bb6a6bb096b17e6c5cbc8861f76e57a5840b6d
Kumar Ojas <44812427+KumarOjas@users.noreply.github.com>
2022-06-12 12:47:13 +0530

    Create 1157A.java

---
+++ 1157A.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+import java.util.Sccanner;
+
+import java.util.Set;
+
+public class Main {
+  
+
+  public static void main (String [] args ){
+    
+    Scanner sc =new Scanner (System.in);
+    
+    int n = sc.nextInt();
+	
+    System.out.println(solve(n));
+
+		sc.close();
+	}
+
+	static int solve(int n) {
+		
+    Set<Integer> history = new HashSet<>();
+		
+    while (!history.contains(n)) {
+		
+      history.add(n);
+			
+      n = f(n);
+		}
+		return history.size();
+	}
+
+	static int f(int x) {
+		
+    int result = x + 1;
+		
+    while (result % 10 == 0) {
+			result /= 10;
+		}
+		return result;
+	}
+}
