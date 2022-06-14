5f3da11b79b49a6cdad28441f604423743928f07
Kumar Ojas <44812427+KumarOjas@users.noreply.github.com>
2022-06-14 21:25:38 +0530

    Create 1199B.java

---
+++ 1199B.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+  
+ 
+  public static void main (String [] args ){
+    
+  
+    Scanner sc = new Scanner (System.in);
+    
+    
+    int H=sc.nextInt();
+    
+    int L=sc.nextInt();
+    
+    System.out.println(Solve(H,l));
+    
+    sc.close ();
+  }
+  
+  static double solve (int H,int L){
+    
+    return ((long) L * L - (long) H * H) / (2.0 * H);
+	}
+}
