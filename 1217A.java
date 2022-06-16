b5ff5e22b952a7d327811ff88173b987e6780533
Kumar Ojas <44812427+KumarOjas@users.noreply.github.com>
2022-06-16 11:18:29 +0530

    Create 1217A.java

---
+++ 1217A.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+  
+  public static void main (String [] args ){
+    
+    SCanner sc = new Scanner (System.in);
+    
+    int T=sc.nextInt();
+    
+    for (int tc = 0; tc < T; tc++) {
+	
+       int str = sc.nextInt();
+			
+       int itl = sc.nextInt();
+			
+       int exp = sc.nextInt();
+
+			System.out.println(solve(str, itl, exp));
+		}
+
+		sc.close();
+	}
+
+	static int solve(int str, int itl, int exp) {
+		return Math.max(0, (str + exp) - Math.max(str, (str + itl + exp + 2) / 2) + 1);
+	}
+}
